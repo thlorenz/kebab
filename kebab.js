0f96497cc7c4d8c7367871d14a562a1a07e0bfad
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-14 22:03:44 -0500

    wrapping inside function to not pollute window

--- kebab.js
+++ kebab.js
@@ -1,26 +1,28 @@
-var slice = Array.prototype.slice;
+(function () {
+  var slice = Array.prototype.slice;
 
-function kebab() {
-  var queue = []
-    , subscriptions = [];
+  function kebab() {
+    var queue = []
+      , subscriptions = [];
 
-  return {
-      enqueue : function () { 
-        queue.push(slice.call(arguments)); 
-        var sub = subscriptions.shift();
-        if (sub) sub.apply(this, queue.shift());
-      }
-    , once    : function (cb) { 
-        var args = queue.shift();
-        return args ? cb.apply(this, args) : subscriptions.push(cb);
-      }
-  };
-}
+    return {
+        enqueue : function () { 
+          queue.push(slice.call(arguments)); 
+          var sub = subscriptions.shift();
+          if (sub) sub.apply(this, queue.shift());
+        }
+      , once    : function (cb) { 
+          var args = queue.shift();
+          return args ? cb.apply(this, args) : subscriptions.push(cb);
+        }
+    };
+  }
 
-if (typeof define === 'function' && define.amd) {
-  define(function () { return kebab; });
-} else if (typeof window === 'object') {
-  window.kebab = kebab;
-} else if (typeof module === 'object' && typeof module.exports === 'object') {
-  module.exports = kebab;
-}
+  if (typeof define === 'function' && define.amd) {
+    define(function () { return kebab; });
+  } else if (typeof window === 'object') {
+    window.kebab = kebab;
+  } else if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = kebab;
+  }
+})();
